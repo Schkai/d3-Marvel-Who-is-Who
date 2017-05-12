e321ad0902f4b892cdd835d7689a373dd0917397
Konstantin Seitz <konstantinseitz@live.de>
2017-05-12 10:24:32 +0200

    Added jQuery, first working api calls.

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,5 +1,39 @@
 "use strict";
+var PRIV_KEY = "PRIVATE_KEY_HERE";
+var PUBLIC_KEY = "PUBLIC_KEY_HERE";
 function draw(data) {
   d3.select("#main").append("circle").attr("cx", 50).attr("cy", 50).attr("r", 50);
 }
 draw();
+function getMarvelResponse() {
+  var ts = new Date().getTime();
+  var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
+  var characterId = '1009718';
+  var url = 'http://gateway.marvel.com:80/v1/public/comics';
+  console.log(url);
+  $.getJSON(url, {
+    ts: ts,
+    apikey: PUBLIC_KEY,
+    hash: hash
+  }).done(function(data) {
+    console.log(data);
+    testImages(data);
+  }).fail(function(err) {
+    console.log(err);
+  });
+}
+;
+function testImages(data) {
+  var results = data.data.results;
+  var resultsLen = results.length;
+  var output = '<ul>';
+  for (var i = 0; i < resultsLen; i++) {
+    if (results[i].images.length > 0) {
+      var imgPath = results[i].images[0].path + '/standard_xlarge.' + results[i].images[0].extension;
+      output += '<li><img src="' + imgPath + '"><br>' + results[i].title + '</li>';
+    }
+  }
+  output += '</ul>';
+  $('#results').append(output);
+}
+getMarvelResponse();
--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,7 @@
+var PRIV_KEY = "PRIVATE_KEY_HERE";
+var PUBLIC_KEY = "PUBLIC_KEY_HERE";
+
+
 function draw(data){
   d3.select("#main")
     .append("circle")
@@ -7,3 +11,54 @@ function draw(data){
 }
 
 draw();
+
+
+function getMarvelResponse() {
+
+  // you need a new ts every request                                                                                    
+  var ts = new Date().getTime();
+  var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
+  
+  // the api deals a lot in ids rather than just the strings you want to use
+  var characterId = '1009718'; // wolverine                                                                             
+
+
+  var url = 'http://gateway.marvel.com:80/v1/public/comics';
+
+  console.log(url);
+  $.getJSON(url, {
+    ts: ts,
+    apikey: PUBLIC_KEY,
+    hash: hash,
+    //characters: characterId
+    })
+    .done(function(data) {
+      // sort of a long dump you will need to sort through
+      console.log(data);
+      testImages(data);
+    })
+    .fail(function(err){
+      // the error codes are listed on the dev site
+      console.log(err);
+    });
+};
+
+function testImages(data){
+      var results = data.data.results;
+      var resultsLen = results.length;
+      var output = '<ul>'; 
+      
+      for(var i=0; i<resultsLen; i++){
+        if(results[i].images.length > 0) {
+          var imgPath = results[i].images[0].path + '/standard_xlarge.' + results[i].images[0].extension;
+          output += '<li><img src="' + imgPath + '"><br>'+results[i].title+'</li>';
+        }
+      }  
+      output += '</ul>'
+      $('#results').append(output);
+}
+
+getMarvelResponse();
+
+
+
